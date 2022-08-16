227bc5621c2ebd4e4ecec13ba84542cbe6c2e22b
Gowtham-M-R <89722325+Gowtham-M-R@users.noreply.github.com>
2022-08-16 22:04:21 +0530

    Time: 42 ms (77.40%), Space: 13.8 MB (99.54%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        l=[]
-        for i in "balon":
-            c=text.count(i)
-            if i == 'l' or i == 'o':
-                c=c//2
-            l.append(c)
-        return min(l)
+#         l=[]
+#         for i in "balon":
+#             c=text.count(i)
+#             if i == 'l' or i == 'o':
+#                 c=c//2
+#             l.append(c)
+        # return min(l)
         
-        
-        # return min(text.count('b'),text.count('a'),text.count('l')//2,text.count('o')//2,text.count('n'))
+
+        return min(text.count('b'),text.count('a'),text.count('l')//2,text.count('o')//2,text.count('n'))
