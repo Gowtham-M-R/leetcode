e367678304230a1a9572934608df184f5e2d714d
Gowtham-M-R <89722325+Gowtham-M-R@users.noreply.github.com>
2022-09-04 22:05:01 +0530

    Time: 126 ms (5.13%), Space: 13.8 MB (64.51%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -2,8 +2,10 @@ class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         s=[]
         for i in range(0,len(words)):
-            for j in range(0,len(words)):
+            for j in range(i+1,len(words)):
                 if words[j] != words[i] and words[j] not in s:
                     if words[j] in words[i]:
                         s.append(words[j])
-        return s
+                    if words[i] in words[j]:
+                        s.append(words[i])
+        return set(s)
