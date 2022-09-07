e8b364c5642e0eb9d132bc1e587e98eb10506129
Gowtham-M-R <89722325+Gowtham-M-R@users.noreply.github.com>
2022-09-07 12:02:43 +0530

    Time: 62 ms (18.25%), Space: 13.9 MB (55.11%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -3,7 +3,7 @@ class Solution:
         t=title.split()
         s=""
         for i in t:
-            if len(i) >= 3:
+            if len(i) > 2:
                 s += i.capitalize()
             else:
                 s+=i.lower()
