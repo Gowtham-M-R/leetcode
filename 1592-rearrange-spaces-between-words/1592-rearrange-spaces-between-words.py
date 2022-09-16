cf487b96371a8f7af2a1bd744725904632b30af5
Gowtham-M-R <89722325+Gowtham-M-R@users.noreply.github.com>
2022-09-16 18:26:11 +0530

    Time: 46 ms (57.59%), Space: 14 MB (16.16%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -5,7 +5,7 @@ class Solution:
         space = text.count(" ")
         text = text.split()
         if len(text) == 1:
-            return "".join(text)+ space * " "
+            return text[0]+ space * " "
         l = space // (len(text)-1)
         rem = space % (len(text)-1)
         return (' ' * l).join(text) + (rem * " ")
